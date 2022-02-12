10027271dcdbb4052b210f64690f1620d0a7d363
Jean Meira <jean.meira10@hotmail.com>
2022-02-12 09:58:29 -0300

    feat: add handles events

--- src/components/TodoListIten/index.tsx
+++ src/components/TodoListIten/index.tsx
@@ -6,11 +6,24 @@ interface Props {
 }
 
 const TodoListIten: React.FC<Props> = ({ todo }: Props) => {
+  const handleChange = () => {
+    console.log('mudou');
+  };
+
+  const handleDelete = (todo: Todo) => {
+    console.log(todo);
+  };
+
   return (
     <tr className="uk-animation-slide-bottom-medium">
       <td className="uk-width-auto">
         <label>
-          <input className="uk-checkbox" type="checkbox" checked={todo.done} />
+          <input
+            className="uk-checkbox"
+            type="checkbox"
+            checked={todo.done}
+            onChange={handleChange}
+          />
         </label>
       </td>
       <td className="uk-width-expand">{todo.title}</td>
@@ -18,6 +31,7 @@ const TodoListIten: React.FC<Props> = ({ todo }: Props) => {
         <button
           className="uk-icon-button uk-button-danger"
           uk-icon="trash"
+          onClick={() => handleDelete(todo)}
         ></button>
       </td>
     </tr>
--- src/models/Todo.tsx
+++ src/models/Todo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 export class Todo {
   constructor(public id: number, public title: string, public done: boolean) {}
 }
